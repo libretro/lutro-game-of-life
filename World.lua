9ce04565ca3aff5650bc6bf9023eafc8a7c09cce
Niouby <alonso.morgane@gmail.com>
2015-02-20 17:54:45 +0100

    add neighborhood function in World class

--- World.lua
+++ World.lua
@@ -6,11 +6,20 @@ World.__index = World
 
 function World.new()
 	local self = setmetatable({}, World)
+	self.lines = 100
+	self.columns = 100
 	self.grid = {}
-	for i=0,99 do
+	for i=0,self.lines-1 do
 		self.grid[i] = {}
-		for j=0,99 do
+		for j=0,self.columns-1 do
 			self.grid[i][j] = Cell.new()
+		end
+	end
+	self.neighbours = {}
+	for i=0,self.lines-1 do
+		self.neighbours[i] = {}
+		for j=0,self.columns-1 do
+			self.neighbours[i][j] = 0
 		end
 	end
 	return self
@@ -36,4 +45,29 @@ function World:draw()
 			lutro.graphics.rectangle(i*6,j*6,5,5, self.grid[i][j]:getColor())
 		end
 	end
+end
+
+function World:neighborhood()
+	for i=0,99 do
+		for j=0,99 do
+			self.neighbours[i][j] = neighbours(i,j)
+		end
+	end	
+end
+
+function World:neighbour(line, column)
+	neighbour = 0;
+	for i=line-1,line+1 do
+		for j=column-1,column+1 do
+			if (i~=line or j~=column) 
+				and i>=0 
+				and j>=0 
+				and i<self.lines 
+				and j<self.columns
+			then
+				neighbour=neighbour+1
+			end
+		end
+	end	
+	return neighbour
 end
